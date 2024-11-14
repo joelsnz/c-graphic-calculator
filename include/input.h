364888e2c8e54ebab98b8cf0f3bff1ea276a5054
joelsnz <joel04slm@gmail.com>
2024-11-14 10:01:21 +0100

    Modify structures

--- include/input.h
+++ include/input.h
@@ -1,15 +1,15 @@
 #ifndef TOUCHSCREEN_H
 #define TOUCHSCREEN_H
 
-#include <stdint.h>
-
 typedef struct {
     uint8_t px;
     uint8_t py;
-} touchScreen;
+}
 
-void initTouchScreen(TouchScreen *ts, uint8_t xPlus, uint8_t xMinus, uint8_t yPlus, uint8_t yMinus);
-void activateTouchScreen(TouchScreen *ts);
-void readCoordinates(TouchScreen *ts);
+#include <stdint.h>
+
+void initTouchScreen(touchScreen *ts, uint8_t xPlus, uint8_t xMinus, uint8_t yPlus, uint8_t yMinus);
+void activateTouchScreen(touchScreen *ts);
+void readCoordinates(touchScreen *ts);
 
 #endif
