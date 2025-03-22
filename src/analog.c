f7f8562943c9ed03c47fe07ce7994fe0e1f2d532
joelsnz <joel04slm@gmail.com>
2025-03-22 11:18:40 +0100

    others

--- src/analog.c
+++ src/analog.c
@@ -1,11 +1,10 @@
 #include "../include/analog.h"
-#include "hal/adc_types.h"
+#include "esp_adc/adc_oneshot.h"
 
 // create adc unit handle under normal oneshot mode form channel 1
-adc_oneshot_unit_handle_t adc1_handle;
-adc_oneshot_unit_init_cfg_t init_config1 = {
-  .unit_id = ADC_UNIT_1,
-  .ulp_mode = ADC_ULP_MODE_DISABLE
+adc_oneshot_unit_handle_t adc2_handle;
+adc_oneshot_unit_init_cfg_t init_config2 = {
+  .unit_id = ADC_UNIT_2,
 };
 ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
 
@@ -14,7 +13,7 @@ adc_oneshot_chan_cfg_t config = {
   .bitwidth = ADC_BITWIDTH_DEFAULT,
   .atten = ADC_ATTEN_DB_12
 };
-ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC_CHAN0, &config));
+ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_2, &config));
 
 // read raw result
 
